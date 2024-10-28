82aa88076638b4ca5c9e99c5c691e5468e311d38
gymmate <ggymmate@gmail.com>
2024-10-28 21:51:07 +0800

    feat: added code

--- frontend/src/pages/SchedulePage.jsx
+++ frontend/src/pages/SchedulePage.jsx
@@ -1,4 +1,6 @@
 import { Box, Flex, Heading, VStack } from "@chakra-ui/react";
+import ScheduleCalendar from "../components/ScheduleCalendar";
+import ScheduleTime from "../components/ScheduleTime";
 
 const SchedulePage = () => {
     return (
@@ -30,7 +32,8 @@ const SchedulePage = () => {
                 bottom={0}
                 p='5%'>
                 <Flex gap='10' align="center" bg="gray.100">
-                    
+                    <ScheduleCalendar/>
+                    <ScheduleTime/>
                 </Flex>
             </Box>
         </VStack>
